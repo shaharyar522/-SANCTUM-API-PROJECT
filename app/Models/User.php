42b894b24ae3f4b1de74cd6aa7c0b07b1853ec9a
shaharyar522 <124666378+shaharyar522@users.noreply.github.com>
2025-08-26 18:40:54 +0500

    api project complete create singup, login, logout please chekc

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,18 +6,19 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasFactory, Notifiable;
+    use HasFactory, Notifiable,HasApiTokens;
 
     /**
      * The attributes that are mass assignable.
      *
      * @var list<string>
      */
-    
+
     protected $fillable = [
         'name',
         'email',
@@ -42,7 +43,7 @@ class User extends Authenticatable
     protected function casts(): array
     {
         return [
-            'email_verified_at' => 'datetime',
+      
             'password' => 'hashed',
         ];
     }
